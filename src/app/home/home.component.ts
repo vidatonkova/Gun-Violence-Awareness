5ddf9b3056ab15aa993825271ebc67fdf0030235
Maanasi Vadlamani <maanasivadlamani@gmail.com>
2020-11-12 13:45:51 -0500

    home basic front-end complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  //stateName: FormGroup;
+  //states: any =['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    
   }
 
+ 
+
+
 }
+/*this.stateName = this.formBuilder.group({
+  selectedState: ['', Validators.required]
+
+   selectState(e){
+    this.stateName.setValue(e.target.value, {onlySelf: true})
+  }
+
+    get f() { return this.stateName.controls; }
+
+})*/
