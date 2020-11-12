4f8a7b1181f42ca50b5c3ccf074ef02ac929e07a
Maanasi Vadlamani <maanasivadlamani@gmail.com>
2020-11-12 16:42:54 -0500

    home page front-end done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { FooterComponent } from './footer/footer.component'
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent, 
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/state-incidents/state-incidents.module.ts
+++ src/app/state-incidents/state-incidents.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { StateIncidentsRoutingModule } from "./state-incidents-routing.module"
 import { StateIncidentsComponent } from "./state-incidents.component"
-import { FooterComponent } from "src/app/footer/footer.component"
 
 @NgModule({
-  declarations: [StateIncidentsComponent, FooterComponent],
+  declarations: [StateIncidentsComponent],
   imports: [
     CommonModule,
     StateIncidentsRoutingModule
