5c49c9e1d66361349850e098a15740eb15ae4af3
ejdog32 <40522897+ejdog32@users.noreply.github.com>
2020-11-21 19:04:27 -0500

    Added map functionality to mass shooting and heap to state

--- src/app/datatransfer.service.ts
+++ src/app/datatransfer.service.ts
@@ -13,7 +13,221 @@ export class DataTransferService {
   constructor(private httpClient: HttpClient) { 
   }
 
-  
+  printValsMap(hash ,x){
+    var notMass = hash.get("false");
+    var mass = hash.get("true");
+    
+    console.log(notMass);
+    console.log(mass);
+    var y = performance.now();
+      var z = y-x;
+      var a = z.toString();
+      console.log(a);
+      localStorage.setItem('maptime', a);
+  }
+
+  massShootingMap(){
+    var d = new Date();
+    var x = performance.now();
+    var isMass = 0;
+    var notMass = 0;
+    var HashMap = require('hashmap');
+    var hash = new HashMap();
+    hash.set("true", 0);
+    hash.set("false", 0);
+    this.httpClient.get("assets/gundata_part1.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9225; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    })
+    this.httpClient.get("assets/gundata_part2.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9225; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    })
+    this.httpClient.get("assets/gundata_part3.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9200; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    })
+    this.httpClient.get("assets/gundata_part4.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9163; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    })
+    this.httpClient.get("assets/gundata_part5.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9132; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    })
+    this.httpClient.get("assets/gundata_part6.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9132; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    })
+    this.httpClient.get("assets/gundata_part7.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9253; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    })
+    this.httpClient.get("assets/gundata_part8.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9384; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    })
+    this.httpClient.get("assets/gundata_part9.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9360; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    })
+    this.httpClient.get("assets/gundata_part10.json").subscribe(data1 =>{
+      console.log("fdkaslf");
+      for(let i = 0; i < 9769; i++){ 
+        if(data1[i].n_killed != undefined){
+
+        if(data1[i].n_killed > 2){
+          var currVal = hash.get("true");
+          hash.delete("true");
+          currVal = currVal + 1;
+          hash.set("true", currVal);
+        }else if(data1[i].n_killed > -1 && data1[i].n_killed < 3){
+          var currVal = hash.get("false");
+          hash.delete("false");
+          currVal = currVal + 1;
+          hash.set("false", currVal);
+        }
+      }
+    }
+    this.printValsMap(hash, x);
+    })
+   
+  }
 
   printVals(heap ,x){
     var notMass = 0;
@@ -162,5 +376,200 @@ export class DataTransferService {
       
       this.printVals(heap, x);
     })
+  }
+
+  printValsWeapHeap(heap ,x){
+      console.log(heap.peek());
+      heap.pop();
+      //go through and look for top three values and store in array as discussed previously
+      var y = performance.now();
+      var z = y-x;
+      var a = z.toString();
+      console.log(a);
+      localStorage.setItem('heaptime', a);
+  }
+
+  weapTypeHeap(){
+    var d = new Date();
+    var x = performance.now();
+    var Heap = require('heap');
+    var heap = new Heap();
+    this.httpClient.get("assets/gundata_part1.json").subscribe(data1 =>{
+      for(let i = 0; i < 9225; i++){ 
+        if(data1[i].gun_type != undefined){
+          heap.push(data1[i].gun_type);
+        }
+    }
+      
+    })
+    this.httpClient.get("assets/gundata_part2.json").subscribe(data2 =>{
+      //console.log(data2); //9164
+      for(let i = 0; i < 9225; i++){ 
+          if(data2[i].gun_type != undefined){
+            heap.push(data2[i].gun_type);
+          }
+    }
+    })
+    this.httpClient.get("assets/gundata_part3.json").subscribe(data3 =>{
+      //console.log(data3); //9200
+      for(let i = 0; i < 9200; i++){ 
+          if(data3[i].gun_type != undefined){
+            heap.push(data3[i].gun_type);
+          }
+    }
+    })
+    this.httpClient.get("assets/gundata_part4.json").subscribe(data4 =>{
+      for(let i = 0; i < 9163; i++){ 
+          if(data4[i].gun_type != undefined){
+            heap.push(data4[i].gun_type);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part5.json").subscribe(data5 =>{
+      for(let i = 0; i < 9132; i++){ 
+          if(data5[i].gun_type != undefined){
+            heap.push(data5[i].gun_type);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part6.json").subscribe(data6 =>{
+      //console.log(data6);
+      for(let i = 0; i < 9132; i++){ 
+          if(data6[i].gun_type != undefined){
+            heap.push(data6[i].gun_type);
+          }
+    }
+    })
+    this.httpClient.get("assets/gundata_part7.json").subscribe(data7 =>{
+      for(let i = 0; i < 9253; i++){ 
+          if(data7[i].gun_type != undefined){
+            heap.push(data7[i].gun_type);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part8.json").subscribe(data8 =>{
+      for(let i = 0; i < 9384; i++){ 
+          if(data8[i].gun_type != undefined){
+            heap.push(data8[i].gun_type);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part9.json").subscribe(data9 =>{
+      for(let i = 0; i < 9360; i++){ 
+          if(data9[i].gun_type != undefined){
+            heap.push(data9[i].gun_type);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part10.json").subscribe(data10 =>{
+      for(let i = 0; i < 9769; i++){ 
+          if(data10[i].gun_type != undefined){
+            heap.push(data10[i].gun_type);
+          }
+      }
+      
+      this.printValsWeapHeap(heap, x);
+    })
+  }
+
+  stateHeap(){
+    var d = new Date();
+    var x = performance.now();
+    var Heap = require('heap');
+    var heap = new Heap();
+    this.httpClient.get("assets/gundata_part1.json").subscribe(data1 =>{
+      for(let i = 0; i < 9225; i++){ 
+        if(data1[i].state != undefined){
+          heap.push(data1[i].state);
+        }
+    }
+      
+    })
+    this.httpClient.get("assets/gundata_part2.json").subscribe(data2 =>{
+      //console.log(data2); //9164
+      for(let i = 0; i < 9225; i++){ 
+          if(data2[i].state != undefined){
+            heap.push(data2[i].state);
+          }
+    }
+    })
+    this.httpClient.get("assets/gundata_part3.json").subscribe(data3 =>{
+      //console.log(data3); //9200
+      for(let i = 0; i < 9200; i++){ 
+          if(data3[i].state != undefined){
+            heap.push(data3[i].state);
+          }
+    }
+    })
+    this.httpClient.get("assets/gundata_part4.json").subscribe(data4 =>{
+      for(let i = 0; i < 9163; i++){ 
+          if(data4[i].state != undefined){
+            heap.push(data4[i].state);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part5.json").subscribe(data5 =>{
+      for(let i = 0; i < 9132; i++){ 
+          if(data5[i].state != undefined){
+            heap.push(data5[i].state);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part6.json").subscribe(data6 =>{
+      //console.log(data6);
+      for(let i = 0; i < 9132; i++){ 
+          if(data6[i].state != undefined){
+            heap.push(data6[i].state);
+          }
+    }
+    })
+    this.httpClient.get("assets/gundata_part7.json").subscribe(data7 =>{
+      for(let i = 0; i < 9253; i++){ 
+          if(data7[i].state != undefined){
+            heap.push(data7[i].state);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part8.json").subscribe(data8 =>{
+      for(let i = 0; i < 9384; i++){ 
+          if(data8[i].state != undefined){
+            heap.push(data8[i].state);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part9.json").subscribe(data9 =>{
+      for(let i = 0; i < 9360; i++){ 
+          if(data9[i].state != undefined){
+            heap.push(data9[i].state);
+          }
+      }
+    })
+    this.httpClient.get("assets/gundata_part10.json").subscribe(data10 =>{
+      for(let i = 0; i < 9769; i++){ 
+          if(data10[i].state != undefined){
+            heap.push(data10[i].state);
+          }
+      }
+      
+      this.printValsStateHeap(heap, x);
+    })
+  }
+
+  printValsStateHeap(heap, x){
+    var stateFound = false;
+    var totalCases = 0;
+    var numInstances = 0;
+    var stateToFind = localStorage.getItem("state");
+    while(heap.size() > 0){
+      if(heap.top() == stateToFind){
+        numInstances++;
+      }
+      heap.pop();
+      totalCases++;
+    }
+    localStorage.setItem("numInstance", numInstances.toString());
+    var percentageOfInc = (numInstances / totalCases) * 100;
+    localStorage.setItem("percentage", percentageOfInc.toString());
+
   }
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   public heaptime;
+  public maptime;
   constructor() {
     this.heaptime = localStorage.getItem('heaptime');
+    this.maptime = localStorage.getItem('maptime');
+
    }
 
   ngOnInit(): void {
