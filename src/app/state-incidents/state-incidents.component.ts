36cc85f492dd304b262f123e89f6690e30306a7b
vidatonkova <vidatonkova@ufl.edu>
2020-11-29 23:27:55 -0500

    changed the graph for state incidents(color + location + size) and fixed the percentage to 5 decimal places. fixed the graph for the mass shootings/incidents(color + location +size) and added in the percent, which is also 5 decimal places

--- src/app/state-incidents/state-incidents.component.ts
+++ src/app/state-incidents/state-incidents.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType } from 'chart.js';
-import { Label, MultiDataSet } from 'ng2-charts';
+import { Label, MultiDataSet, Colors } from 'ng2-charts';
 
 @Component({
   selector: 'app-state-incidents',
@@ -12,16 +12,35 @@ export class StateIncidentsComponent implements OnInit {
   public numInc;
   public percentage;
 
-  public doughnutChartLabels: Label[] = [localStorage.getItem('state'), 'Not'];
-  public doughnutChartData: MultiDataSet = [
+  public doughnutChartLabels: Label[] = [localStorage.getItem('state'), 'US Cases'];
+  
+  public doughnutChartData: 
+  
+  MultiDataSet = [
     [Number(localStorage.getItem('numInstance')), Number(localStorage.getItem('totalCases'))]
   ];
+
+  public doughnutChartColors: Colors[] = [
+    
+    {backgroundColor:["#ffb347","#FF5800","#FFB414"],
+    borderColor:["#000000","#000000"]},
+    
+    //"#9E120E"
+  ];
+  
   public doughnutChartType: ChartType = 'pie';
+
+  public doughnutChartOptions:any = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
 
   constructor() { 
     //this.state = localStorage.getItem('state');
     //this.numInc = localStorage.getItem('numInstance');
-    //this.percentage = localStorage.getItem('percentage');
+   // this.percentage = localStorage.getItem('percentage');
+    //this.percentage = (this.percentage).toFixed(5);
+   // console.log(this.percentage);
   }
 
   ngOnInit(): void {
@@ -29,7 +48,11 @@ export class StateIncidentsComponent implements OnInit {
     
     this.state = localStorage.getItem('state');
     this.numInc = localStorage.getItem('numInstance');
-    this.percentage = localStorage.getItem('percentage');
+    var temp = localStorage.getItem('percentage');
+    var temp2 = parseFloat(temp);
+    this.percentage = (temp2).toFixed(5)
+  
+    console.log(this.percentage);
     // if (!localStorage.getItem('autoLoad')) { 
     //   localStorage.setItem('autoLoad', 'no reload') 
     //   location.reload() 
